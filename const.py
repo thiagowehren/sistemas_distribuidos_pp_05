b242a17abd730a016a1cbca19162fec6c456bc19
Thiago Wehren <thiago-ws@hotmail.com>
2022-09-16 19:34:29 -0300

    Update const.py

--- const.py
+++ const.py
@@ -1,5 +1,5 @@
-CHAT_SERVER_HOST = "localhost"
+CHAT_SERVER_HOST = "172.31.85.232"
 CHAT_SERVER_PORT = 5001
 
 # IDs de usuários registrados com os respectivos endereços IP e números de porta
-registry = {"Alice":("localhost",5002), "Bob":("localhost",5003)}
+registry = {"Alice":("172.31.94.58",5002), "Bob":("172.31.80.41",5003)}
